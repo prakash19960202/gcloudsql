4d737492a91737a275f27981f48051323726dc40
prakash19960202 <136340051+prakash19960202@users.noreply.github.com>
2023-07-10 18:31:39 +0530

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,20 @@
+resource "google_sql_database_instance" "instance" {
+  name             = "instance1"
+  region           = "us-central1"
+  database_version = "SQLSERVER_2017_EXPRESS"
+  root_password    = "Prakash@123"
+  settings {
+    tier = "db-custom-2-7680"
+     ip_configuration {
+      authorized_networks {
+        name         = "Allow Local IP"
+        value        = "171.76.82.126"
+        
+      }
+    }
+  }
+  # set `deletion_protection` to true, will ensure that one cannot accidentally delete this instance by
+  # use of Terraform whereas `deletion_protection_enabled` flag protects this instance at the GCP level.
+  deletion_protection = false
+}
+
